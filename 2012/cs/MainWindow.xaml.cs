c8cda37e3ab7cf0a96f7bb38c4cbbc01b57984c6
branch331 <34431991+branch331@users.noreply.github.com>
2018-10-02 01:24:37 -0500

    Implemented Code Review Feedback
    •	Make a default path if blank
    o	Populate the control
    •	Set HTML as the default instead of XML
    •	For file browsing, set explicit file types: XML, HTML, zip, etc depending on selection
    •	‘Report Type’: header above the control
    •	Change wording: ‘Determine’ Report Type => ‘Select’
    •	Screen Size: has a lot of real estate
    o	Can add space between entries/buttons this way 
    o	Smaller overall window

--- 2012/cs/MainWindow.xaml.cs
+++ 2012/cs/MainWindow.xaml.cs
@@ -15,38 +15,49 @@ namespace NationalInstruments.Examples.GenerateMAXReport
         }
 
         private ReportWorker worker;
+        private string reportTypeExtension;
 
         private void OnBrowseClick(object sender, RoutedEventArgs e)
         {
+            ReturnReportType();
             OpenFileDialog fileExplorer = new OpenFileDialog();
             fileExplorer.Title = "Choose a report directory and filename";
             fileExplorer.CheckFileExists = false;
+            // Restrict file type of file dialog. For instance, the filter for XML is: "XML files (*.xml)|*.xml".
+            fileExplorer.Filter = reportTypeExtension + " files (*." + reportTypeExtension + ")|*." + reportTypeExtension; 
             fileExplorer.ShowDialog();
             filePathBox.Text = fileExplorer.FileName;
         }
 
         private void OnGenerateReportClick(object sender, RoutedEventArgs e)
         {
-
-            if (ReportTypeBox.Text == "HTML")
-            {
-                worker.ReportType = ReportType.Html;
-            }
-            else if (ReportTypeBox.Text == "Technical Support")
-            {
-                worker.ReportType = ReportType.TechnicalSupportZip;
-            }
-            else
-            {
-                worker.ReportType = ReportType.Xml;
-            }
-
+            worker.ReportType = ReturnReportType();
+           
             if (string.IsNullOrEmpty(filePathBox.Text))
             {
-                filePathBox.Text = System.IO.Path.GetTempPath() + "Report" + worker.ReportType; //FIX THIS
+                filePathBox.Text = System.IO.Path.GetTempPath() + "MAXReport." + reportTypeExtension; 
             }
 
             worker.GenerateReport(passwordBox.Password);
+        }
+
+        private ReportType ReturnReportType()
+        {
+            if (ReportTypeBox.Text == "XML")
+            {
+                reportTypeExtension = "xml";
+                return ReportType.Xml;
+            }
+            else if (ReportTypeBox.Text == "Technical Support")
+            {
+                reportTypeExtension = "zip";
+                return ReportType.TechnicalSupportZip;
+            }
+            else
+            {
+                reportTypeExtension = "html";
+                return ReportType.Html;
+            }
         }
     }
 }
